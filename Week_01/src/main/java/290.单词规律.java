1ccdbe938ff3e4ef65ecfe5631924621a7b4e23d
gengke <geng_ke@sina.com>
2020-05-23 14:56:01 +0800

    完成290题

--- Week_01/src/main/java/290.单词规律.java
+++ Week_01/src/main/java/290.单词规律.java
@@ -1,6 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution290 {
-    public boolean wordPattern(String pattern, String str) {
+    public boolean wordPattern1(String pattern, String str) {
         String[] words = str.split(" ");
+        if (pattern.length() != words.length) return false;
+
         for (int i = 0; i < pattern.length(); i++) {
             if (pattern.indexOf( pattern.charAt(i)) != indexArrayOf(words, words[i])) {
                 return false;
@@ -16,5 +21,18 @@ class Solution290 {
         }
 
         return  -1;
+    }
+
+    public boolean wordPattern2(String pattern, String str) {
+        String[] words = str.split(" ");
+        if (pattern.length() != words.length) return false;
+
+        Map map = new HashMap();
+
+        for (Integer i = 0; i < words.length; i++) {
+            if (map.put(pattern.charAt(i), i) != map.put(words[i], i)) return false;
+        }
+
+        return true;
     }
 }
--- Week_01/src/test/java/Solution290Test.java
+++ Week_01/src/test/java/Solution290Test.java
@@ -4,8 +4,43 @@ import org.junit.Test;
 public class Solution290Test {
 
     @Test
-    public void wordPattern() {
+    public void wordPatternTest1() {
         Solution290 solution290 = new Solution290();
-        Assert.assertTrue(solution290.wordPattern("abba", "dog cat cat dog"));
+        String pattern = "", str = "";
+        pattern = "abba"; str = "dog cat cat dog";
+        Assert.assertTrue(solution290.wordPattern1(pattern, str));
+
+        pattern = "abba"; str = "dog cat cat fish";
+        Assert.assertFalse(solution290.wordPattern1(pattern, str));
+
+        pattern = "aaaa"; str = "dog cat cat dog";
+        Assert.assertFalse(solution290.wordPattern1(pattern, str));
+
+        pattern = "abba"; str = "dog dog dog dog";
+        Assert.assertFalse(solution290.wordPattern1(pattern, str));
+        pattern = "ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccdd";
+        str = "s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s t t";
+        Assert.assertTrue(solution290.wordPattern1(pattern, str));
+    }
+
+    @Test
+    public void wordPatternTest2() {
+        Solution290 solution290 = new Solution290();
+        String pattern = "", str = "";
+        pattern = "abba"; str = "dog cat cat dog";
+        Assert.assertTrue(solution290.wordPattern2(pattern, str));
+
+        pattern = "abba"; str = "dog cat cat fish";
+        Assert.assertFalse(solution290.wordPattern2(pattern, str));
+
+        pattern = "aaaa"; str = "dog cat cat dog";
+        Assert.assertFalse(solution290.wordPattern2(pattern, str));
+
+        pattern = "abba"; str = "dog dog dog dog";
+        Assert.assertFalse(solution290.wordPattern2(pattern, str));
+
+        pattern = "ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccdd";
+        str = "s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s t t";
+        Assert.assertTrue(solution290.wordPattern2(pattern, str));
     }
 }
