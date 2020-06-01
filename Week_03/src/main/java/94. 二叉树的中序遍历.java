0e069d98656d4067073026872d1ae5ae5d3a0ef9
gengke <geng_ke@sina.com>
2020-06-01 17:18:42 +0800

    完成94题

---
+++ Week_03/src/main/java/94. 二叉树的中序遍历.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution94 {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+
+        helper(root, result);
+    }
+
+    private void helper(TreeNode root, List<Integer> result) {
+        if (root == null) {
+            return;
+        }
+
+        helper(root.left, result);
+        result.add(root.val);
+        helper(root.right, result);
+    }
+}
