a2f135c9b6c2d0fecc7b15d993b4fef928cf4be3
gengke <geng_ke@sina.com>
2020-06-05 07:19:39 +0800

    完善面试题04

---
+++ Week_03/src/main/java/面试题 04. 检查平衡性.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null){
+            return true;
+        }
+        if (Math.abs(depth(root.left)-depth(root.right))>1){
+            return false;
+        }
+
+        return isBalanced(root.right) && isBalanced(root.left);
+    }
+
+    public int depth(TreeNode root) {
+        if (root == null) return 0;
+
+        return Math.max(depth(root.left), depth((root.right))) + 1;
+    }
+}
