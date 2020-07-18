5f69f405380cb2dca6a92d708bcb9a2cd6e5b63d
gengke <geng_ke@sina.com>
2020-07-18 14:38:00 +0800

    完成344题

---
+++ Week_09/src/main/java/344. 反转字符串.java
@@ -0,0 +1,7 @@
+class Solution344 {
+    public void reverseString(char[] s) {
+        for (int i = 0, j = s.length -1; i < j; i++, j--) {
+            char tmp = s[i]; s[i] = s[j]; s[j] = tmp;
+        }
+    }
+}
